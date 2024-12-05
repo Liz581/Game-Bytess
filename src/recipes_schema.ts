ca72d2a23039944e380203702d00f062ec8ed50e
Lisset Rico <lrico5@uic.edu>
2024-12-05 09:07:07 -0600

    more pages, buttons for filtering, file for collection setups

---
+++ src/recipes_schema.ts
@@ -0,0 +1,21 @@
+// confirm the setup
+
+export interface Recipe {
+    id: string;
+    name: string;
+    game: string;
+    description: string;
+    ingredients: string;
+    steps: string
+    imageURL: string;
+    likes: number;
+}
+
+export interface remixRecipe {
+    id: string;
+    recipe_id: number;
+    user_uid: string;
+    changes: string;
+    imageURL: string;
+    likes: number;
+}
