e750b7a9c297e1cfba95e4de8e0fe022bcc041a7
Ciara Taylor <ctaylo42@uic.edu>
2024-12-05 10:58:37 -0600

    added "i remixed this" buttons to recipe cards

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import styles from './RecipeCard.module.css';
+import React from "react";
+import styles from "./RecipeCard.module.css";
 
 function RecipeCard({ title, game, image }) {
   return (
-    <div className={styles['recipe-card']}>
+    <div className={styles["recipe-card"]}>
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <p>{game}</p>
+      <button className={styles["button"]} type="submit">
+        I remixed this!
+      </button>
+      <p></p>
     </div>
   );
 }
