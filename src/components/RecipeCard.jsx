047a395fc9df439858ab9e3f933a403cae7d111d
Lisset Rico <lrico5@uic.edu>
2024-12-06 14:58:12 -0600

    updated recipe card

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -7,10 +7,10 @@ function RecipeCard({ title, game, image }) {
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <p>{game}</p>
+      
       <button className={styles["button"]} type="submit">
         I remixed this!
       </button>
-      <p></p>
     </div>
   );
 }
